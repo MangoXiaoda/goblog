5e60b9e84f8f7937da77ffbdf2cbf6dc4bbc74c6
lizhongda <944872615@qq.com>
2021-11-17 16:59:58 +0800

    设置标头

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
 	// fmt.Fprint(w, "请求路径为："+r.URL.Path)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello，欢迎来到 goblog！</h1>")
 	} else if r.URL.Path == "/about" {
