8115b89f067066792daa4e8b458f21b664e546af
lizhongda <944872615@qq.com>
2021-11-17 16:07:18 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
+	// fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
+	// fmt.Fprint(w, "请求路径为："+r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+
+			"<a href=\"https://www.baidu.com\">百度一下")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
 
 func main() {
