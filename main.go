ccb1842d7974ea0c6d4fdd691644146ba1cec8c0
lizhongda <944872615@qq.com>
2021-11-22 15:41:47 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -3,25 +3,63 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
-	// fmt.Fprint(w, "请求路径为："+r.URL.Path)
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello，欢迎来到 goblog！</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+
-			"<a href=\"https://www.baidu.com\">百度一下")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我们。</p>")
-	}
-}
+// func defaultHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset-utf-8")
+// 	if r.URL.Path == "/" {
+// 		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+// 	} else {
+// 		w.WriteHeader(http.StatusNotFound)
+// 		fmt.Fprint(w, "<h1>请求页面未找到：（</h1>"+
+// 			"<p>如有疑惑，请联系我们。</p>")
+// 	}
+// }
+
+// func aboutHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset-utf-8")
+// 	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+// 		"<a href=\"http://www.baidu.com\">mangoDa")
+// }
+
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// 	// fmt.Fprint(w, "<h1>Hello，这里是 goblog</h1>")
+// 	// fmt.Fprint(w, "请求路径为："+r.URL.Path)
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	if r.URL.Path == "/" {
+// 		fmt.Fprint(w, "<h1>Hello，欢迎来到 goblog！</h1>")
+// 	} else if r.URL.Path == "/about" {
+// 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系"+
+// 			"<a href=\"https://www.baidu.com\">百度一下")
+// 	} else {
+// 		w.WriteHeader(http.StatusNotFound)
+// 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+// 			"<p>如有疑惑，请联系我们。</p>")
+// 	}
+// }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	// router.HandleFunc("/", defaultHandler)
+	// router.HandleFunc("/about", aboutHandler)
+
+	// 文章详情
+	router.HandleFunc("/articles/", func(w http.ResponseWriter, r *http.Request) {
+		id := strings.SplitN(r.URL.Path, "/", 3)[2]
+		fmt.Fprint(w, "文章 ID："+id)
+	})
+
+	// 列表 or 创建
+	router.HandleFunc("/articles", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprint(w, "访问文章列表")
+		case "POST":
+			fmt.Fprint(w, "创建新的文章")
+		}
+	})
+
+	http.ListenAndServe(":3000", router)
 }
