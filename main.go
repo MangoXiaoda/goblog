b52b34ceb3656c7a43e9f96b1bded495c18ab522
lzd <944872615@qq.com>
2021-11-24 22:09:09 +0800

    调整模板文件

--- main.go
+++ main.go
@@ -86,29 +86,6 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "body 的长度为：%v <br>", utf8.RuneCountInString(body))
 	} else {
 
-		html := `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<title>创建文章 —— 我的技术博客</title>
-			<style type="text/css">.error {color: red;}</style>
-		</head>
-		<body>
-			<form action="{{ .URL }}" method="post">
-				<p><input type="text" name="title" value="{{ .Title }}"></p>
-				{{ with .Errors.title }}
-				<p class="error">{{ . }}</p>
-				{{ end }}
-				<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
-				{{ with .Errors.body }}
-				<p class="error">{{ . }}</p>
-				{{ end }}
-				<p><button type="submit">提交</button></p>
-			</form>
-		</body>
-		</html>
-		`
-
 		storeURL, _ := router.Get("articles.store").URL()
 
 		data := ArticlesFormData{
@@ -118,7 +95,7 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 			Errors: errors,
 		}
 
-		tmpl, err := template.New("create-form").Parse(html)
+		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
 		if err != nil {
 			panic(err)
 		}
@@ -153,24 +130,24 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
+// 文章创建表单页面
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-	html := `
-	<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<title>创建文章 —— 我的技术博客</title>
-	</head>
-	<body>
-		<form action="%s?test=data" method="post">
-			<p><input type="text" name="title"></p>
-			<p><textarea name="body" cols="30" rows="10"></textarea></p>
-			<p><button type="submit">提交</button></p>
-		</form>
-	</body>
-	</html>
-	`
+
 	storeURL, _ := router.Get("articles.store").URL()
-	fmt.Fprintf(w, html, storeURL)
+
+	data := ArticlesFormData{
+		Title:  "",
+		Body:   "",
+		URL:    storeURL,
+		Errors: nil,
+	}
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl.Execute(w, data)
 }
 
 func main() {
