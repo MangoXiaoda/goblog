c9a727ea88e5afa1761074a14e0c3e4f0ee61683
lizhongda <944872615@qq.com>
2021-12-10 11:55:04 +0800

    重构文章列表页面

---
+++ app/models/article/article.go
@@ -0,0 +1,11 @@
+package article
+
+import (
+	"goblog/pkg/route"
+	"strconv"
+)
+
+// Link 方法用来生成文章链接
+func (article Article) Link() string {
+	return route.Name2URL("articles.show", "id", strconv.FormatUint(article.ID, 10))
+}
--- main.go
+++ main.go
@@ -26,48 +26,6 @@ type Article struct {
 	ID          int64
 }
 
-// Link 方法用来生成文章链接
-func (a Article) Link() string {
-	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(a.ID, 10))
-	if err != nil {
-		logger.LogError(err)
-		return ""
-	}
-	return showURL.String()
-}
-
-// 文章首页列表
-func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-
-	// 1、执行查询语句，返回一个结果集
-	rows, err := db.Query("SELECT * FROM articles")
-	logger.LogError(err)
-	defer rows.Close()
-
-	var articles []Article
-	// 2、循环读取结果
-	for rows.Next() {
-		var article Article
-		// 2.1 扫描每一行的结果并赋值到一个 article 对象中
-		err := rows.Scan(&article.ID, &article.Title, &article.Body)
-		logger.LogError(err)
-
-		// 2.2 将 article 追加到 articles 的这个数组中
-		articles = append(articles, article)
-	}
-
-	// 2.3 检测遍历时是否发生错误
-	err = rows.Err()
-	logger.LogError(err)
-
-	// 3、加载模板
-	tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
-	logger.LogError(err)
-
-	// 4、渲染模板，将所有文章的数据传输进去
-	tmpl.Execute(w, articles)
-}
-
 // 通过文章ID获取文章数据
 func getArticleByID(id string) (Article, error) {
 	article := Article{}
@@ -429,7 +387,6 @@ func main() {
 	router = bootstrap.SetupRoute()
 
 	// 文章相关
-	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 
 	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
