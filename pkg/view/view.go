d83ecf7701f90b7abcd265c6b494a7d9980076ac
lizhongda <944872615@qq.com>
2021-12-29 17:48:16 +0800

    注册页面

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -9,8 +9,21 @@ import (
 	"strings"
 )
 
-// Render 渲染视图
+// D 是 map[string]interface{} 的简写
+type D map[string]interface{}
+
+// Render 渲染通用视图
 func Render(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "app", data, tplFiles...)
+}
+
+// RenderSimple 渲染简单的视图
+func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+	RenderTemplate(w, "simple", data, tplFiles...)
+}
+
+// RenderTemplate 渲染视图
+func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
 	// 1 设置模板相对路径
 	viewDir := "resources/views/"
 
@@ -23,7 +36,7 @@ func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	layoutFiles, err := filepath.Glob(viewDir + "layouts/*.gohtml")
 	logger.LogError(err)
 
-	// 4 在 Slice 里新增我们的目标文件
+	// 4 合并所有文件
 	allFiles := append(layoutFiles, tplFiles...)
 
 	// 5 解析所有模板文件
@@ -34,6 +47,6 @@ func Render(w io.Writer, data interface{}, tplFiles ...string) {
 	logger.LogError(err)
 
 	// 6 渲染模板
-	err = tmpl.ExecuteTemplate(w, "app", data)
+	err = tmpl.ExecuteTemplate(w, name, data)
 	logger.LogError(err)
 }
