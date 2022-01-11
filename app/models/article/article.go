1031acde88d6f78b63bafafc898c00b84e81a1e0
lizhongda <944872615@qq.com>
2022-01-11 11:32:49 +0800

    文章作者

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -2,6 +2,7 @@ package article
 
 import (
 	"goblog/app/models"
+	"goblog/app/models/user"
 	"goblog/pkg/route"
 	"strconv"
 )
@@ -10,11 +11,18 @@ import (
 type Article struct {
 	models.BaseModel
 
-	Title string `gorm:"type:varchar(255);not null;" valid:"title"`
-	Body  string `gorm:"type:longtext;not null;" valid:"body"`
+	Title  string `gorm:"type:varchar(255);not null;" valid:"title"`
+	Body   string `gorm:"type:longtext;not null;" valid:"body"`
+	UserID uint64 `gorm:"not null;index"`
+	User   user.User
 }
 
 // Link 方法用来生成文章链接
 func (article Article) Link() string {
 	return route.Name2URL("articles.show", "id", strconv.FormatUint(article.ID, 10))
+}
+
+// CreateAtDate 创建日期
+func (article Article) CreatedAtDate() string {
+	return article.CreatedAt.Format("2006-01-02")
 }
