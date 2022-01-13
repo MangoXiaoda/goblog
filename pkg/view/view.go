583c53c642d5d8a1248635405ce771747bac54a3
lizhongda <944872615@qq.com>
2022-01-13 14:24:55 +0800

    显示作者

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -1,6 +1,7 @@
 package view
 
 import (
+	"goblog/app/models/user"
 	"goblog/pkg/auth"
 	"goblog/pkg/flash"
 	"goblog/pkg/logger"
@@ -30,6 +31,7 @@ func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string) {
 	data["isLogined"] = auth.Check()
 	data["loginUser"] = auth.User
 	data["flash"] = flash.All()
+	data["Users"], _ = user.All()
 
 	// 2、生成模板文件
 	allFiles := getTemplateFiles(tplFiles...)
