084f89f9d04f5baa83cac5d3577171265c706460
lizhongda <944872615@qq.com>
2022-01-14 15:52:19 +0800

    关联文章和分类

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -11,10 +11,11 @@ import (
 type Article struct {
 	models.BaseModel
 
-	Title  string `gorm:"type:varchar(255);not null;" valid:"title"`
-	Body   string `gorm:"type:longtext;not null;" valid:"body"`
-	UserID uint64 `gorm:"not null;index"`
-	User   user.User
+	Title      string `gorm:"type:varchar(255);not null;" valid:"title"`
+	Body       string `gorm:"type:longtext;not null;" valid:"body"`
+	UserID     uint64 `gorm:"not null;index"`
+	CategoryID uint64 `gorm:"not null;default:4;index"`
+	User       user.User
 }
 
 // Link 方法用来生成文章链接
